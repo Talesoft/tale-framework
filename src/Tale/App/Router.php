b70bb950102dd2a387dc435af751c7b4af4e22a0
Torben Köhn <t.koehn@outlook.com>
2015-09-04 13:33:55 +0200

    Refactored: Router. Also fixed the callable-TypeHint bug

--- src/Tale/App/Router.php
+++ src/Tale/App/Router.php
@@ -2,74 +2,83 @@
 
 namespace Tale\App;
 
-class Router {
+class Router
+{
 
 	private $_routes;
 
-	public function __construct( array $routes = null ) {
+	public function __construct(array $routes = null)
+	{
 
 		$this->_routes = $routes ? $routes : [];
 	}
 
-	public function setRoute( $route, callable $handler ) {
+	public function setRoute($route, $handler)
+	{
 
-		if( $handler instanceof \Closure )
-			$handler = $handler->bindTo( $this, $this );
+		if (!is_callable($handler))
+			throw new \InvalidArgumentException(
+				"Argument 2 passed to Router->setRoute needs to be"
+				."a valid PHP-style callback"
+			);
 
-		$this->_routes[ $route ] = $handler;
+		$this->_routes[$route] = $handler;
 
 		return $this;
 	}
 
-	public function route( $string ) {
+	public function route($string)
+	{
 
-		foreach( $this->_routes as $route => $handler )
-			if( $result = $this->match( $route, $string ) )
-				if( ( $result = call_user_func( $handler, $result ) ) !== false )
+		foreach ($this->_routes as $route => $handler)
+			if ($result = $this->match($route, $string))
+				if (($result = call_user_func($handler, $result)) !== false)
 					return $result;
 
 		return null;
 	}
 
-	protected function getRegExFromRoute( $route ) {
+	protected function getRegExFromRoute($route)
+	{
 
-		return '/^'.preg_replace_callback( '#(.)?:([a-z\_]\w*)(\?)?#i', function( $m ) {
+		return '/^'.preg_replace_callback('#(.)?:([a-z\_]\w*)(\?)?#i', function ($m) {
 
-			$key = $m[ 2 ];
+			$key = $m[2];
 			$initiator = '';
 			$optional = '';
 			$dot = '.';
 
-			if( !empty( $m[ 1 ] ) ) {
+			if (!empty($m[1])) {
 
-				$initiator = '(?<'.$key.'Initiator>'.preg_quote( $m[ 1 ], '/' ).')';
+				$initiator = '(?<'.$key.'Initiator>'.preg_quote($m[1], '/').')';
 
-				if( $m[ 1 ] === $dot )
+				if ($m[1] === $dot)
 					$dot = '';
 			}
 
-			if( !empty( $m[ 3 ] ) )
+			if (!empty($m[3]))
 				$optional = '?';
 
 
 			return '(?:'.$initiator.'(?<'.$key.'>[a-z0-9\_\-'.$dot.']*?))'.$optional;
 
-		}, $route ).'$/i';
+		}, $route).'$/i';
 	}
 
-	protected function match( $route, $string ) {
+	protected function match($route, $string)
+	{
 
 		$matches = [];
-		$isMatch = preg_match( $this->getRegExFromRoute( $route ), $string, $matches );
+		$isMatch = preg_match($this->getRegExFromRoute($route), $string, $matches);
 
-		if( !$isMatch )
+		if (!$isMatch)
 			return false;
 
 		$vars = [];
-		if( !empty( $matches ) )
-			foreach( $matches as $name => $value )
-				if( is_string( $name ) && !empty( $value ) )
-					$vars[ $name ] = $value;
+		if (!empty($matches))
+			foreach ($matches as $name => $value)
+				if (is_string($name) && !empty($value))
+					$vars[$name] = $value;
 
 		return $vars;
 	}
